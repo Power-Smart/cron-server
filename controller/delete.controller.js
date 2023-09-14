9f6fc87440a2067019cecd92607dcfe35e5cc60d
kavishka-sulakshana <kss20001218@gmail.com>
2023-09-14 14:20:23 +0530

    update in delete schedule functionality

--- controller/delete.controller.js
+++ controller/delete.controller.js
@@ -1,17 +1,16 @@
-import {jobs, files} from "../init.js";
+import { jobs, files } from "../init.js";
 import axios from 'axios';
 import cron from 'node-cron';
 
 
-export const deleteJobs = async (req,res) => {
+export const deleteJobs = async (req, res) => {
 
-    try{
-        const {scheduleId} = req.body;
+    try {
+        const { scheduleId } = req.body;
 
-        console.log("StartJobs", jobs.jobListStart);
-        console.log("StopJobs", jobs.jobListStop);
-
-        if(jobs.jobListStart.hasOwnProperty(scheduleId) && jobs.jobListStop.hasOwnProperty(scheduleId)){
+        if (jobs.jobListStart.hasOwnProperty(scheduleId) && jobs.jobListStop.hasOwnProperty(scheduleId)) {
+            const start = jobs.jobListStart[scheduleId];
+            const stop = jobs.jobListStop[scheduleId];
 
             delete jobs.jobListStart[scheduleId];
             delete jobs.jobListStop[scheduleId];
@@ -19,16 +18,18 @@ export const deleteJobs = async (req,res) => {
             // files.deleteEntry(scheduleId, "startJobs");
             // files.deleteEntry(scheduleId, "stopJobs");
 
-            if(!files.deleteEntry(scheduleId, "startJobs") || !files.deleteEntry(scheduleId, "stopJobs")){
+            if (!files.deleteEntry(scheduleId, "startJobs") || !files.deleteEntry(scheduleId, "stopJobs")) {
                 throw new Error("File not deleted");
             }
+            start.stop();
+            stop.stop();
 
-        }else{
+        } else {
             throw new Error("No job found");
         }
         res.status(200).send("Job Deleted");
 
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
         res.status(500).send(error.message);
     }
